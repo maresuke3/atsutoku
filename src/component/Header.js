741091c438250d54a7ef85c302c00bf57c90ef23
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-01-31 19:27:44 +0900

    ヘッダーのメニューをMainMenuに変更

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,22 +3,20 @@ import React from 'react';
 // import {Link} from 'react-router-dom'; /* ルーティング時に使用するLinkをReactRouterよりインポート */
 import Atsutoku from './img/hot-sale.svg'; /* サービスのロゴマーク */
 import "./css/header.css";
-// import Mainmenu from './MainMenu';
-import DetailMenu from './DetailMenu';
+import Mainmenu from './MainMenu';
+// import DetailMenu from './DetailMenu';
 
 function Header() {
     
-    // 機能
-
     return(
         <header className="header">
-            <div className="serviceIcon">
+            <a href="/" className="serviceIcon">
                 <img className="header-logo" src={Atsutoku} alt="Atsutoku"/>
-                <h1 className="serviceName"><a href="/">AtsuToku</a></h1>
-            </div>
+                <h1 className="serviceName">AtsuToku</h1>
+            </a>
             <div className="menu">
-                {/*<Mainmenu />*/}
-                <DetailMenu />
+                {/* <DetailMenu /> */}
+                <Mainmenu />
             </div>
         </header>
     );
