98ed5c1834c56a517fe820cbb6f2c6f284c5d4d6
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-01-31 16:24:36 +0900

    商品詳細画面のヘッダーメニューを作成

---
+++ src/component/DetailMenu.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+import map from './img/map.svg';
+import './css/header.css';
+
+export default function DetailMenu() {
+
+    /* shop名をステートで管理しステートからshop名を表示させる */
+    const [shop, setShop] = useState('選択した店が表示されます');
+    const shopChange = event => {
+        console.log(shop);
+    }
+
+    return(
+        <div className="detailList">
+            <ul className="detail-contents">
+                <li className="shopName">{shop}</li>
+                <li><a href="https://www.google.co.jp/maps?hl=ja&tab=rl&authuser=0"><img className="mapbtn" src={map} alt="GoogleMap"/></a></li>
+            </ul>
+        </div>
+    )
+}
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,10 @@
 // import
-import React, { useState } from 'react';
+import React from 'react';
 // import {Link} from 'react-router-dom'; /* ルーティング時に使用するLinkをReactRouterよりインポート */
 import Atsutoku from './img/hot-sale.svg'; /* サービスのロゴマーク */
 import "./css/header.css";
-import Mainmenu from './MainMenu';
+// import Mainmenu from './MainMenu';
+import DetailMenu from './DetailMenu';
 
 function Header() {
     
@@ -13,10 +14,11 @@ function Header() {
         <header className="header">
             <div className="serviceIcon">
                 <img className="header-logo" src={Atsutoku} alt="Atsutoku"/>
-                <h1 className="serviceName">AtsuToku</h1>
+                <h1 className="serviceName"><a href="/">AtsuToku</a></h1>
             </div>
             <div className="menu">
-                <Mainmenu />
+                {/*<Mainmenu />*/}
+                <DetailMenu />
             </div>
         </header>
     );
