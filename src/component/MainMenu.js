05d7dd173853bbb4d11e864aa4d58ec1aee5aff1
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-02-01 16:38:37 +0900

    現在地を取得、検索範囲の指定を作成

--- src/component/MainMenu.js
+++ src/component/MainMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/header.css';
 
 export default function MainMenu() {
@@ -9,12 +9,45 @@ export default function MainMenu() {
     const changeGenru = event => {
         setGenru(event.target.value);
     }
+
     /* 検索範囲の距離をstateで管理 */
     const [distance, setDistance] = useState(0);
     const changeDistance = event => {
         setDistance(event.target.value);
     }
 
+    /* 範囲検索機能 */
+    /* 中心座標を取得する */
+    const [center, setCenter] = useState()              //中心座標をstateで管理する
+
+    /* 中心座標を取得できた際に走るメソッド */
+    const getPosition = data => {
+        const currentPosition = {
+            lat: data.coords.latitude,
+            lng: data.coords.longitude
+        };
+        setCenter(currentPosition);
+        console.log(center);
+    }
+
+    /* 中心座標が取得できなかった際に走るメソッド */
+    const unfindPosition = data => {
+        const currentPosition = {
+            lat: 34.673542,
+            lng: 135.43338
+        };
+        setCenter(currentPosition);
+        console.log(center);
+    }
+
+    /* geolvationによる現在地の座標を取得 */
+    useEffect(() => {
+        /* getCurrentPositionの第一引数が現在地を取得できた際に走り、第二引数が現在地情報取得拒否またはエラーが出た時に走る */
+        navigator.geolocation.getCurrentPosition(getPosition, unfindPosition);
+    },[]) ;
+
+    
+    
     return(
         <ul className="menu-list">
             <li className="menu-contents">
